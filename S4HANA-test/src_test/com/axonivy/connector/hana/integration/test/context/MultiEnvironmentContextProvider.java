cfea7537c454ea57971797eaf509a5800177df85
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-31 11:30:09 +0700

    fix typo

--- S4HANA-test/src_test/com/axonivy/connector/hana/integration/test/context/MultiEnvironmentContextProvider.java
+++ S4HANA-test/src_test/com/axonivy/connector/hana/integration/test/context/MultiEnvironmentContextProvider.java
@@ -20,9 +20,9 @@ public class MultiEnvironmentContextProvider implements TestTemplateInvocationCo
 		String testEnv = System.getProperty(S4HanaTestConstants.END_TO_END_TESTING_ENVIRONMENT_KEY);
 		return switch (testEnv) {
 		case S4HanaTestConstants.END_TO_END_TESTING_ENVIRONMENT_VALUE ->
-			Stream.of(new TestEnironmentInvocationContext(S4HanaTestConstants.REAL_CALL_CONTEXT_DISPLAY_NAME));
+			Stream.of(new TestEnvironmentInvocationContext(S4HanaTestConstants.REAL_CALL_CONTEXT_DISPLAY_NAME));
 		default ->
-			Stream.of(new TestEnironmentInvocationContext(S4HanaTestConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+			Stream.of(new TestEnvironmentInvocationContext(S4HanaTestConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
 		};
 	}
 }
