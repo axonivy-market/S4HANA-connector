dabf5e1992507a7c4e96a333e29dc9b5bb42ac0b
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-08-20 17:29:47 +0700

    MARP-2631 Remove unused codes

--- S4HANA-test/src_test/com/axonivy/connector/hana/integration/test/constants/S4HanaTestConstants.java
+++ S4HANA-test/src_test/com/axonivy/connector/hana/integration/test/constants/S4HanaTestConstants.java
@@ -1,10 +1,6 @@
 package com.axonivy.connector.hana.integration.test.constants;
 
 public class S4HanaTestConstants {
-	public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
-	public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
 	public static final String BASE_URL = "baseUrl";
 	public static final String API_KEY = "apiKey";
-	public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
-	public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }
